1d998f83e170740b6b4d0eef9a8a6112456a0b1a
Wataru Nishimura <nishimura@mikan-foundation.org>
2025-08-31 16:41:07 +0900

    list up file-baed mdx as links

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,7 +1,33 @@
+import Link from 'next/link';
+import { readdirSync, statSync } from 'node:fs';
+import { join } from 'node:path';
+
+function walk(dir: string, baseRoute = ''): string[] {
+  return readdirSync(dir).flatMap((file) => {
+    const fullPath = join(dir, file);
+    const stat = statSync(fullPath);
+
+    if (stat.isDirectory()) {
+      return file;
+    }
+
+    return '';
+  });
+}
+
 export default function DocsPage() {
+  const dirs = walk('app/docs');
+
   return (
     <main>
       <h1>n13u Documentation</h1>
+      <ul>
+        {dirs.map((dir) => (
+          <li key={dir}>
+            <Link href={'/docs/' + dir}>{dir}</Link>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
