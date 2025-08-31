5713d19279f10f46f2dee4f423a3396930a12f5e
Wataru Nishimura <nishimura@mikan-foundation.org>
2025-08-31 15:19:22 +0900

    add markdown pref and formatter pref

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+};
+
+export default config;
